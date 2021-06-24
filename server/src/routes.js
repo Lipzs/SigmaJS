eee98965ab8f7cc67ab524024bea867236323660
Lipzs <filipedias823@hotmail.com>
2021-06-24 19:40:47 -0300

    atualização nas validações

--- server/src/routes.js
+++ server/src/routes.js
@@ -5,7 +5,6 @@ import QuestionController from './controllers/QuestionController.js'
 import corsMiddleware from './middlewares/corsMiddleware.js';
 import authMiddleware from './middlewares/authMiddleware.js';
 import fieldsMiddleware from './middlewares/fieldsMiddleware.js';
-import fieldsMiddlewareLogin from './middlewares/fieldsMiddlewareLogin.js';
 
 const routes = express.Router();
 
@@ -13,7 +12,7 @@ const userController = new UserController()
 const questionController = new QuestionController();
 
 routes.post("/signup", corsMiddleware, fieldsMiddleware, userController.createUser);
-routes.post("/login", corsMiddleware, fieldsMiddlewareLogin ,userController.userLogin);
+routes.post("/login", corsMiddleware, fieldsMiddleware ,userController.userLogin);
 
 routes.post("/answers", corsMiddleware, authMiddleware, questionController.postAnswers);
 routes.get('/getRanking', corsMiddleware, authMiddleware, questionController.getRank);
