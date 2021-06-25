ea8f93ae57b7914c879aa1f509658250e21d8c9f
Jardel Urban <jardelurban3@gmail.com>
2021-06-24 21:29:01 -0300

    ranking

--- web/src/pages/home/index.jsx
+++ web/src/pages/home/index.jsx
@@ -1,20 +1,26 @@
-import React from 'react'
-import Header from '../../components/header';
-import { Container } from '@chakra-ui/react'
+import React from "react";
+import Header from "../../components/header";
+import { Container } from "@chakra-ui/react";
+import RankingGroup from "../../components/ranking-group";
 
-import './styles.css'
+import "./styles.css";
 
-export default function Home(){
-  return(
+export default function Home() {
+  return (
     <>
       <Header />
       <Container centerContent className="homeContainer">
+        <h2 id="rank-title">RANKING</h2>
+        <div class="ranking-box">
+          <RankingGroup></RankingGroup>
+          <RankingGroup></RankingGroup>
+          <RankingGroup></RankingGroup>
+          <RankingGroup></RankingGroup>
+          <RankingGroup></RankingGroup>
+        </div>
         <div className="buttonsContainer">
           <div className="play">
             <h1>JOGAR</h1>
-          </div>
-          <div className="rank">
-            <h1>RANKING</h1>
           </div>
         </div>
       </Container>
